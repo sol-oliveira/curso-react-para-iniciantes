5e92cd2d3ad6824b5cd2b48c83240d6579474539
sol-oliveira <71611273+sol-oliveira@users.noreply.github.com>
2021-04-11 14:54:10 -0300

    Initial commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./navbar.css";
+
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <span>React Kanban</span>
+    </nav>
+  );
+}
